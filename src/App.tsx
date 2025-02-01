206e65be5f371229ebc34afec9ecd592d7d3d0cd
AlexPal005 <rudenkoolexandr06@knu.ua>
2025-02-01 21:23:35 +0200

    fix: change router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Route, Routes } from 'react-router'
+import { Route, Routes } from 'react-router-dom'
 import { Users } from './pages/users/ui/Users.tsx'
 import { SavedUsers } from './pages/saved/ui/SavedUsers.tsx'
 
